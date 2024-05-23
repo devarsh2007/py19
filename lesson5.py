7eee5eb33934c6141ad5483da77ce55dc1e0aaf7
devarsh2007 <sunilpatel2131@gmail.com>
2024-05-23 09:32:57 +0530

    code

---
+++ lesson5.py
@@ -0,0 +1,10 @@
+# write a program that find bmi
+
+height = float(input("enter height : "))
+weight = float(input("enter weight : "))
+
+height=height/100
+
+bmi = weight/(height*height)
+
+print("bmi : ",bmi)
---
+++ operator1.py
@@ -0,0 +1,10 @@
+a=20.4
+
+print(a//2.5) #floor division
+
+b=30.234
+c=12
+print(b//c)
+
+a=11.9
+print(a//2)
