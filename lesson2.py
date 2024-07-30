25c63a117b2340c77c8caa28f8e5d842e50dc32e
devarsh2007 <sunilpatel2131@gmail.com>
2024-07-30 08:46:46 +0530

    code

---
+++ lesson2.py
@@ -0,0 +1,20 @@
+print("hello world")
+print("-------------------------")
+print()
+print("welcome to python....")
+
+print('python')
+
+print("""
+      this is first line
+      this is second line
+      this is third line 
+      """)
+
+print('''
+      this is 
+            multiple 
+                  line message....
+      ''')
+
+print("12345");print("6789")
