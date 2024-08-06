6b498d7c025ca644c1d95fdf60f2e8092830b9ce
devarsh2007 <sunilpatel2131@gmail.com>
2024-08-06 08:58:03 +0530

    string

---
+++ lesson4.py
@@ -0,0 +1,27 @@
+number = 30003.5
+print("number :\n",number)
+
+t = type(number)
+print("type of \nnumber : ",t)
+# print("")
+# print("")
+# print("\n\n")
+print(type(number))
+
+# string
+name = "devarsh patel12425_+)"
+print(type(name))
+print("\n",name)
+
+name = "devarsh"
+print(name)
+
+surname = "patel"
+print(surname)
+
+# string concatination
+name = "\nMr. "+name+" "+surname
+print(name)
+
+# repeation of string
+print(name*5)
