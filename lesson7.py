a46915af95809dd642f8a476fa3748307edcde68
devarsh2007 <sunilpatel2131@gmail.com>
2024-08-09 08:54:47 +0530

    input

---
+++ lesson7.py
@@ -0,0 +1,12 @@
+name = input("enter your name : ")
+
+print(name)
+print(type(name))
+
+number = int(input("enter a number : "))
+print(number)
+print(type(number))
+
+marks = float(input("enter marks : "))
+print(marks)
+print(type(marks))
---
+++ lesson8.py
@@ -0,0 +1,15 @@
+# write a program that perform addiiton,subtraction,division,multiplication
+
+a = float(input("enter number1 : "))
+b = float(input("enter number2 : "))
+
+# c = a+b
+print("\n\naddition : ",a+b)
+print("subtraction : ",a-b)
+print("multiplication : ",a*b)
+print("division : ",a/b)
+
+print("-----------------------------------------")
+name = str(input("enter message : "))
+print("string : ",name)
+print("reverse string : ",name[::-1])
