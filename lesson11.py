521931db6c928238ec768c0e7169315793815504
devarsh2007 <sunilpatel2131@gmail.com>
2024-08-14 09:00:07 +0530

    operator

--- lesson11.py
+++ lesson11.py
@@ -4,4 +4,3 @@ c = float(input("enter Celsius : "))
 f = ((9/5) * c) + 32
 print(f"celsius : {c} °C")
 print(f"ferenhit : {f} °F")
- 
---
+++ operator1.py
@@ -0,0 +1,21 @@
+# arithmetic operator
+a=40
+b=20
+
+print(a+b)
+print(a-b)
+print(a*b)
+
+print(a/b)
+print(a%b)
+
+print(5%2)
+print(5/2)
+
+print(10/5) 
+print(10%5)
+print(12%3)
+
+print(2**3)
+print(4**1)
+print(10**12)
