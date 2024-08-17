e49402ef1989911bbc4ccc688cd4b8d65c3b966e
devarsh2007 <sunilpatel2131@gmail.com>
2024-08-17 08:56:41 +0530

    list

---
+++ list1.py
@@ -0,0 +1,14 @@
+employee = ["devarsh",17,48.57,True] 
+print(employee)
+
+employee2 = [19,23.7,"param"]
+print(employee2)
+
+print("name : ",employee[0])
+print(employee2[2])
+print(employee[1])
+print(employee[3])
+
+print(employee2+employee)
+print(employee[0]+" "+employee2[2])
+print(employee[0]+str(employee2[0])) # type casting
