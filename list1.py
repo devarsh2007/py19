8e85172cb319f91ae813ef63a3f65eb0d98b9c0f
devarsh2007 <sunilpatel2131@gmail.com>
2024-08-20 09:08:13 +0530

    list

--- list1.py
+++ list1.py
@@ -12,3 +12,24 @@ print(employee[3])
 print(employee2+employee)
 print(employee[0]+" "+employee2[2])
 print(employee[0]+str(employee2[0])) # type casting
+
+print("--------------------------------------------")
+numbers = [10,20,30,40,50,60,70,80,90,100]
+print(len(numbers))
+
+print(numbers[::-10])
+print(numbers[5:])
+print(numbers[0::2])
+
+print(numbers*5)
+
+print("------------------------------------------------")
+
+city = ["Ahmedabad", "Surat", "Vadodara", "Rajkot", "Bhavnagar", "Jamnagar", "Junagadh", "Gandhinagar", "Anand", "Nadiad"]
+print(city)
+
+city.append("amreli")
+print(city)
+
+city.insert(5,"godhra")
+print(city)
